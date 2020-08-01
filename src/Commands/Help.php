beb2cf1a8788d709749eb9a7c7e3c6d3a8a62ada
Yakov <txncii@gmail.com>
2020-08-01 20:05:07 +0300

    help command fixed

--- src/Commands/Help.php
+++ src/Commands/Help.php
@@ -11,6 +11,6 @@ class Help extends Command implements CommandInterface
 			"/help - show available commands\n" .
 			"/list - show list of added sites\n" .
 			"/add - add site, e.g. <i>/add 127.0.0.1 or /add 127.0.0.1, 127.0.0.2 domain.com</i>\n" .
-			"/del - remove site, e.g. <i>/del domain.com or /del 127.0.0.1, 127.0.0.2<i>";
+			"/del - remove site, e.g. <i>/del domain.com or /del 127.0.0.1, 127.0.0.2</i>";
 	}
 }
